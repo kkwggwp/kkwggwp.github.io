cd206b1e112f168dc1327939fe16c3f74bc4550d
kkwggwp <gkandpwpjp@gmail.com>
2024-08-26 16:35:11 +0900

    또 수정

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('click', (event) => {
 
 var container = document.getElementById('kakaomap'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+	center: new kakao.maps.LatLng(33.44241793094654, 126.57149784381792), //지도의 중심좌표.
 	level: 3 //지도의 레벨(확대, 축소 정도)
 };
 
@@ -68,7 +68,7 @@ var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리
 // kakao map user control
 var mapContainer = document.getElementById('kakaomap'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(33.44241793094654, 126.57149784381792), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };  
 
