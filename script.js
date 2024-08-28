1dd655e8600258e722d0d636e9fed4fa96e7017c
kkwggwp <gkandpwpjp@gmail.com>
2024-08-28 14:15:32 +0900

    map 수정 4

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('click', (event) => {
 
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(33.44241793094654, 126.57149784381792), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };  
 
@@ -90,26 +90,18 @@ function zoomOut() {
     map.setLevel(map.getLevel() + 1);
 }
 
-// //kakao map marker
+//kakao map marker
 
-// var mapContainer = document.getElementById('kakaomap'), // 지도를 표시할 div 
-//     mapOption = { 
-//         center: new kakao.maps.LatLng(33.44241793094654, 126.57149784381792), // 지도의 중심좌표
-//         level: 3 // 지도의 확대 레벨
-//     };
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(33.44241793094654, 126.57149784381792); 
 
-// var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
 
-// // 마커가 표시될 위치입니다 
-// var markerPosition  = new kakao.maps.LatLng(33.44241793094654, 126.57149784381792); 
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
 
-// // 마커를 생성합니다
-// var marker = new kakao.maps.Marker({
-//     position: markerPosition
-// });
-
-// // 마커가 지도 위에 표시되도록 설정합니다
-// marker.setMap(map);
-
-// // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
-// // marker.setMap(null);
+// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null);
